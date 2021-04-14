1e2e14b8e687d34d35893dc8dccd012f2ced04ac
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-04-14 10:08:42 +0200

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,123 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="bg-dark text-white">
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title class="text-uppercase text-grey-5">
+          Germanpsycho
+          <span class="text-weight-light">Admin</span>
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <!-- <q-list>
+        <q-item-label header class="text-grey-8">
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list> -->
+
+      <q-item-label header class="text-grey-8"> Menü </q-item-label>
+
+      <q-list padding>
+        <q-item clickable to="/" exact v-ripple>
+          <q-item-section avatar>
+            <q-icon name="flutter_dash" />
+          </q-item-section>
+          <q-item-section>Home</q-item-section>
+        </q-item>
+
+        <q-item clickable to="/kommentare" exact v-ripple>
+          <q-item-section avatar>
+            <q-icon name="grading" />
+          </q-item-section>
+          <q-item-section>Kommentare</q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+// import EssentialLink from 'components/EssentialLink.vue'
+
+// const linksData = [
+//   {
+//     title: 'Home',
+//     caption: 'quasar.dev',
+//     icon: 'school',
+//     link: '#'
+//   },
+//   {
+//     title: 'Kommentare editieren',
+//     caption: 'github.com/quasarframework',
+//     icon: 'code',
+//     link: '/#/kommentare'
+//   },
+//   {
+//     title: 'Discord Chat Channel',
+//     caption: 'chat.quasar.dev',
+//     icon: 'chat',
+//     link: 'https://chat.quasar.dev'
+//   },
+//   {
+//     title: 'Forum',
+//     caption: 'forum.quasar.dev',
+//     icon: 'record_voice_over',
+//     link: 'https://forum.quasar.dev'
+//   },
+//   {
+//     title: 'Twitter',
+//     caption: '@quasarframework',
+//     icon: 'rss_feed',
+//     link: 'https://twitter.quasar.dev'
+//   },
+//   {
+//     title: 'Facebook',
+//     caption: '@QuasarFramework',
+//     icon: 'public',
+//     link: 'https://facebook.quasar.dev'
+//   },
+//   {
+//     title: 'Quasar Awesome',
+//     caption: 'Community Quasar projects',
+//     icon: 'favorite',
+//     link: 'https://awesome.quasar.dev'
+//   }
+// ]
+
+export default {
+  name: 'MainLayout',
+  // components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false
+      // essentialLinks: linksData
+    }
+  }
+}
+</script>
---
+++ src/pages/EditComments.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <q-card-section>
+        comments-seite
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditComments'
+}
+</script>
+
+<style></style>
